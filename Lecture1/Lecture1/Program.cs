eb1ffd0f953c49584c9354417c53cae55f6d0c33
Grant Erickson <grant@IntelliTect.com>
2022-01-11 16:09:24 -0800

    Add Stuff

--- Lecture1/Lecture1/Program.cs
+++ Lecture1/Lecture1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Inigo Montoya";
+        Guid guid = Guid.NewGuid();
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Inigo Montoya: " + text);
+    }
+
+
+}
+
