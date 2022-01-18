473a6b80ee79de560c52df8f575b9c21335f33ab
Mark Michaelis <mark@IntelliTect.com>
2022-01-18 14:33:48 -0800

    Renamed directories

---
+++ Lecture/Lecture/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Lecture1
+{
+    public class Person
+    {
+        (string, string)[] Passwords = new[] { 
+            ("Inigo Montoya", "YouKilledMyF@ther!")
+        }; 
+
+        public bool Login(string userName, string password)
+        {
+            return password == "YouKilledMyF@ther!";
+        }
+    }
+}
---
+++ Lecture/Lecture/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Inigo Montoya";
+        Guid guid = Guid.NewGuid();
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Inigo Montoya: " + text);
+    }
+
+
+}
+
