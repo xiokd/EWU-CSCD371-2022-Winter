937c4b1cf2070a0076fe8b390407fe4b8d2e1b5c
Mark Michaelis <mark@IntelliTect.com>
2022-02-01 15:59:31 -0800

    Added exception code

--- Lecture/ExceptionHandlingProgram/Program.cs
+++ Lecture/ExceptionHandlingProgram/Program.cs
@@ -1,9 +1,54 @@
-﻿Console.WriteLine("Hello, World!");
-
-Console.Write("Enter a number");
-string? input = Console.ReadLine();
-if (input is not null)
+﻿
+class Program
 {
-    int number = int.Parse(input);
-    Console.WriteLine($"The value is: {number} ");
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Hello, World!");
+            GetNumberFromConsole();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+            // throw;
+        }
+    }
+
+    private static void GetNumberFromConsole()
+    {
+        int? number = null;
+        while (number is null)
+        {
+            Console.Write("Enter a number: ");
+            string? input = Console.ReadLine();
+            if (input is not null)
+            {
+                number=EnterNumber(number, input);
+            }
+            else
+            {
+                Console.WriteLine("Quit");
+                return;
+            }
+        }
+    }
+
+    private static int? EnterNumber(int? number, string input)
+    {
+        try
+        {
+            number = int.Parse(input);
+            Console.WriteLine($"The value is: {number} ");
+        }
+        catch (Exception exception) // Need to change this exception type.
+        {
+            throw new ArgumentException(
+                message:"Input is not a valid integer.", 
+                paramName:nameof(input),
+                innerException: exception);
+        }
+
+        return number;
+    }
 }
