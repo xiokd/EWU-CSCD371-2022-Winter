da169eb43b036b445ac562aa9bffb125ff466578
Mark Michaelis <mark@IntelliTect.com>
2022-02-01 15:33:16 -0800

    Added ExceptionConsoleProgram

---
+++ Lecture/ExceptionHandlingProgram/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.Write("Enter a number");
+string? input = Console.ReadLine();
+if (input is not null)
+{
+    int number = int.Parse(input);
+    Console.WriteLine($"The value is: {number} ");
+}
