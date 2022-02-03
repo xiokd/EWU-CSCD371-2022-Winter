0ed095feae15cf582212545cb6b6b9a3d384d442
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-02-02 22:49:27 -0800

    Added PrintToConsoleServiceTests. Revised project file and Print method.

---
+++ CanHazFunny/CanHazFunny.Tests/PrintToConsoleServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace CanHazFunny.Tests
+{
+    [TestClass]
+    public class PrintToConsoleServiceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Print_WithNullInput_ThrowsException()
+        {
+            var printToConsoleService = new PrintToConsoleService();
+            printToConsoleService.Print(null!);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Print_WithEmptyStringInput_ThrowsException()
+        {
+            var printToConsoleService = new PrintToConsoleService();
+            printToConsoleService.Print("");
+        }
+
+    }
+}
--- CanHazFunny/CanHazFunny/PrintToConsoleService.cs
+++ CanHazFunny/CanHazFunny/PrintToConsoleService.cs
@@ -7,6 +7,9 @@ namespace CanHazFunny
 
         public void Print(string text)
         {
+            if(text is null) { throw new ArgumentNullException("Parameter text is null"); }
+            if(text == string.Empty) { throw new ArgumentException("Parameter text is empty"); }
+
             Console.WriteLine(text);
         }
     }
