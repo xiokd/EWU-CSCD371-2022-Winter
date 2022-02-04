178eced4d6839bc8a186966d54be8e35d8b47d99
Daylyn Hoxie <dhoxie@eagles.ewu.edu>
2022-02-03 18:46:16 -0800

    Fixed empty string issue

--- CanHazFunny/CanHazFunny/PrintToConsoleService.cs
+++ CanHazFunny/CanHazFunny/PrintToConsoleService.cs
@@ -8,6 +8,8 @@ namespace CanHazFunny
         public void Print(string text)
         {
             if (text == null) throw new ArgumentNullException(nameof(text));
+            if (text.Length == 0) throw new ArgumentException(nameof(text));
+
             Console.WriteLine(text);
         }
     }
