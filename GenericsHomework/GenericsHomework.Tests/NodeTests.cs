dce77a48fa7b58d15155c145d6c5c085c266af09
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-02-07 15:39:53 -0800

    Initial setup

---
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsHomework.Tests
+{
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
