f09f950e7555c0d75dad12c2569b2e4bb43b71f1
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-02-07 20:03:56 -0800

    Added constructor, ToString, Append, and Exists methods to Node class

--- GenericsHomework/GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -5,6 +5,9 @@ namespace GenericsHomework.Tests
     [TestClass]
     public class NodeTests
     {
+
+        // TODO: Implement tests
+
         [TestMethod]
         public void TestMethod1()
         {
--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,7 +1,62 @@
 ﻿namespace GenericsHomework
 {
-    public class Node
+    public class Node<T> where T : notnull
     {
+        private T Value { get; set; }
+        public Node<T> Next { get; private set; }
 
+        public Node(T value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public override string? ToString()
+        {
+            return Value.ToString();
+        }
+
+        public void Append(T value)
+        {
+            if(Exists(value)) { throw new ArgumentException("The value already exists"); }
+
+            Node<T> node = new(value);
+            Node<T> currentNext = this.GetNext();
+
+            while(currentNext.GetNext() != this) { currentNext = currentNext.GetNext(); }
+
+            currentNext.SetNext(node);
+            node.SetNext(this);
+        }
+
+        public void Clear()
+        {
+            // TODO: implement Clear() method
+        }
+
+        public bool Exists(T value)
+        {
+            if(this.Value.Equals(value)) { return true; }
+
+            Node<T> currentNext = this.GetNext();
+
+            while(currentNext != this)
+            {
+                if(currentNext.Value.Equals(value)) { return true; }
+                currentNext = currentNext.GetNext();
+            }
+
+            return false;
+        }
+
+        public Node<T> GetNext()
+        {
+            return this.Next;
+        }
+
+        public void SetNext(Node<T> next)
+        {
+            this.Next = next;
+        }
     }
 }
