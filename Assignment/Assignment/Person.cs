1b1045ed86e29e8f1b40ebf9ddbb127ecfbdf382
Mark Michaelis <mark@IntelliTect.com>
2022-02-15 14:53:51 -0800

    Minor change to validate build

--- Assignment/Assignment/Person.cs
+++ Assignment/Assignment/Person.cs
@@ -17,5 +17,6 @@ namespace Assignment
         public string LastName { get; set; }
         public IAddress Address { get;set; }
         public string EmailAddress { get; set; }
+        // Inocuous change
     }
 }
