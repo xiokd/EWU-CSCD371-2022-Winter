14c69f34cdd428c84e541505befc4581fdf498d4
Grant Erickson <grant@IntelliTect.com>
2022-03-01 15:14:11 -0800

    API Call

--- LectureWeb/js/index.js
+++ LectureWeb/js/index.js
@@ -1,12 +1,12 @@
 var name = "Inigo Montoya";
 var person = {
     name: "Inigo Montoya",
-    age: function() {
+    age: function () {
         return new Date().getFullYear() - this.year;
-    }, 
+    },
     year: 1987,
     weapon: "sword",
-    getName: function() {
+    getName: function () {
         return this.name;
     }
 };
@@ -16,6 +16,21 @@ function showName() {
 }
 console.log("Hello World: " + person.getName() + " is " + person.age());
 
-document.getElementById("mybutton").addEventListener("click", function(){
+document.getElementById("mybutton").addEventListener("click", function () {
     console.log("Hello World: " + person.getName() + " is " + person.age());
 });
+
+axios({
+    method: 'get',
+    url: 'https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=47.658&lon=-117.402&appid=e44ff6715c0c7c9bf43ce3bfe74fdb6f'
+})
+    .then(function (response) {
+        console.log(response);
+        console.log(response.data.main.temp);
+
+        let weatherText = document.querySelector(".weather");
+        weatherText.innerText = "Current Temp: " + response.data.main.temp;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
