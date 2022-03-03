c1d9b3ca1d969cad0d19d6d43eccb23a46c45ab7
Grant Erickson <grant@IntelliTect.com>
2022-03-03 14:42:44 -0800

    Adding wordle

---
+++ WordleApp/WordleApp/Controllers/WordleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WordleApp.Controllers
+{
+    [Route("api/wordle")]
+    [ApiController]
+    public class WordleController : ControllerBase
+    {
+        public static List<string> Words { get; } = new()
+        {
+            
+        };
+    }
+}
