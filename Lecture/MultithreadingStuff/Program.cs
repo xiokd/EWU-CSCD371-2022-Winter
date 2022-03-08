eab1fb4ed3cfee4eeceff2cd206433ce6b8ec1ab
Mark Michaelis <mark@IntelliTect.com>
2022-03-08 14:57:53 -0800

    Created a simple Task

--- Lecture/MultithreadingStuff/Program.cs
+++ Lecture/MultithreadingStuff/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Task.Run(
+            () => Display('.')
+            );
+
+        Console.WriteLine("ENTER to exit");
+        Console.ReadLine();
+    }
+    static void Display(char character)
+    {
+        while (true)
+        {
+            Console.Write(character);
+        }
+    }
+}
+
