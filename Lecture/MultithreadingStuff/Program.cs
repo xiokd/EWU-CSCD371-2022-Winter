39ce7cc11077574655a8b12aaf6ad71dfb464d5f
Mark Michaelis <mark@IntelliTect.com>
2022-03-08 16:12:34 -0800

    Shared static data

--- Lecture/MultithreadingStuff/Program.cs
+++ Lecture/MultithreadingStuff/Program.cs
@@ -1,28 +1,35 @@
 ﻿
 public class Program
 {
+    static int iterationCount = 0;
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-
         CancellationTokenSource cancellationTokenSource = 
             new CancellationTokenSource();
-        Task task = Task.Run(
-            () => Display('.', cancellationTokenSource.Token)
-            );
 
+        Task<int> task = Task.Run(
+            () => Display('.', cancellationTokenSource.Token)                
+            );
+        
+        iterationCount--;
         Console.WriteLine("ENTER to exit");
         Console.ReadLine();
-        Console.WriteLine("Thanks for letting me done writing boring periods.... sheesh.. give me some real work.");
         cancellationTokenSource.Cancel();
+        iterationCount = task.Result;
+        Console.WriteLine($"IterationCount = {iterationCount}");
         Console.WriteLine("Exiting!!!");
     }
-    static void Display(char character, CancellationToken cancellationToken)
+    static int Display(char character, CancellationToken cancellationToken)
     {
+        iterationCount = 0;
+        Thread.CurrentThread.Name = "DisplayThread";
         while (!cancellationToken.IsCancellationRequested)
         {
             Console.Write(character);
+            iterationCount++;
         }
+        return iterationCount;
     }
 }
 
