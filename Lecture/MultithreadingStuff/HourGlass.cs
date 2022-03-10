47fb404bd292d93b8a7663e7a0086d46d2c614a3
Mark Michaelis <mark@IntelliTect.com>
2022-03-10 14:12:17 -0800

    Added HourGlass class

---
+++ Lecture/MultithreadingStuff/HourGlass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingStuff;
+
+public class HourGlass
+{
+    public int Display(char character, CancellationToken cancellationToken)
+    {
+        int iterationCount = 0;
+        Thread.CurrentThread.Name = "DisplayThread";
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Console.Write(character);
+            iterationCount++;
+        }
+        return iterationCount;
+    }
+}
