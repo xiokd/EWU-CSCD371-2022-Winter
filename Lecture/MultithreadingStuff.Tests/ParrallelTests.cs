e24a7aaa2985b2af8989d93dc0b12fcc9b81a47f
Mark Michaelis <mark@IntelliTect.com>
2022-03-10 18:41:35 -0800

    Code at the end of class

--- Lecture/MultithreadingStuff.Tests/ParrallelTests.cs
+++ Lecture/MultithreadingStuff.Tests/ParrallelTests.cs
@@ -1,4 +1,9 @@
-﻿namespace MultithreadingStuff.Tests;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Parallel;
+
+namespace MultithreadingStuff.Tests;
 
 [TestClass]
 public class ParrallelTests
@@ -6,6 +11,13 @@ public class ParrallelTests
     [TestMethod]
     public void MyTestMethod()
     {
+        
+        IEnumerable<string> members = 
+            typeof(Enumerable).GetMembers().Select(item=>item.Name);
 
+        IEnumerable<string> membersBeginningWithS =
+            members.Where(item => item[0] == 'S');
+
+       
     }
 }
